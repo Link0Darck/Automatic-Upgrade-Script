55a31036e994071b28e842a9f6ecc90b5a00d7ad
Link_Darck <28543376+Link0Darck@users.noreply.github.com>
2023-09-26 13:26:32 +0200

    Update Raspberry-Pi.sh

--- Raspberry-Pi.sh
+++ Raspberry-Pi.sh
@@ -6,10 +6,10 @@
 
 clear;
 
-echo "#####################################";
-echo "# Script Mise à Jour De Link_Darck  #";
-echo "# Website : http://linkdarck.fr.nf/ #";
-echo "#####################################";
+echo "##############################################";
+echo "# Script Mise à Jour De Link_Darck           #";
+echo "# Website : http://linkdarck.neko-world.ovh/ #";
+echo "##############################################";
 
 sleep 5;
 clear;
